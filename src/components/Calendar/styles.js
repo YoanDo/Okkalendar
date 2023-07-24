909fc1231a14ccda6694c784cae8e333fc3bdb65
Yo <y.n.dorny@gmail.com>
2023-07-24 10:38:49 +0200

    feat(calendar): initial daypilot calendar

---
+++ src/components/Calendar/styles.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const CalendarWrapper = styled.div`
+/* calendar event */
+.calendar_default_event_inner {
+    background: #2e78d6;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    font-size: 10pt;
+    padding: 5px;
+    opacity: 0.8;
+}
+`
