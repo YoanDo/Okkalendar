67bd436c1b36d6cd740da1cd319f2c038ba24f59
Yo <y.n.dorny@gmail.com>
2023-07-25 00:46:04 +0200

    feat(api): adding basic api

---
+++ src/pages/api/events.js
@@ -0,0 +1,47 @@
+let events = [
+  {
+    id: 1,
+    text: 'Surf',
+    start: '2023-07-25T12:30:00',
+    end: '2023-07-25T13:30:00',
+  },
+  {
+    id: 2,
+    text: 'Reu Bernard',
+    start: '2023-07-25T09:30:00',
+    end: '2023-07-25T11:30:00',
+  },
+  {
+    id: 3,
+    text: 'Atelier',
+    start: '2023-07-26T12:00:00',
+    end: '2023-07-26T15:00:00',
+  },
+  {
+    id: 4,
+    text: 'Dej Client',
+    start: '2023-07-28T11:30:00',
+    end: '2023-07-28T14:30:00',
+  },
+];
+
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    // Handle GET request to fetch all events
+    res.status(200).json(events);
+  } else if (req.method === 'POST') {
+    // Handle POST request to add a new event
+    const newEvent = req.body;
+    newEvent.id = events.length + 1;
+    events.push(newEvent);
+    res.status(201).json(newEvent);
+  } else if (req.method === 'DELETE') {
+    // Handle DELETE request to remove an event
+    const { id } = req.body;
+    events = events.filter((event) => event.id !== id);
+    res.status(200).json({ message: 'Event deleted successfully' });
+  } else {
+    // Handle other types of requests
+    res.status(405).end();
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'styled-components';
+import dynamic from 'next/dynamic'; // Import next/dynamic
+import theme from '@/assets/theme';
+
+// Dynamically import the Calendar component with CSR
+const DynamicCalendar = dynamic(() => import('@/containers/Calendar'), {
+  ssr: false, // Disable SSR for this component
+});
+
+export default function Home() {
+  return (
+    <ThemeProvider theme={theme}>
+      <main>
+        <DynamicCalendar /> {/* Render the Calendar component */}
+      </main>
+    </ThemeProvider>
+  );
+}
